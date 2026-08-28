0164ed3f8d494776a92ff8419453a21d08313992
serinanogit <studymonique829@gmail.com>
2026-08-28 21:28:33 +0800

    Use first-person wording throughout handbook

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const boxes=document.querySelectorAll('input[type="checkbox"][data-save]');boxes.forEach(box=>{const key='penghu_'+box.dataset.save;if(localStorage.getItem(key)==='true')box.checked=true;box.addEventListener('change',()=>localStorage.setItem(key,box.checked));});
+const boxes=document.querySelectorAll('input[type="checkbox"][data-save]');
+boxes.forEach(box=>{
+  const key='penghu_'+box.dataset.save;
+  if(localStorage.getItem(key)==='true') box.checked=true;
+  box.addEventListener('change',()=>localStorage.setItem(key,box.checked));
+});
+
+// 手冊以第一人稱撰寫：把頁面中殘留的「你」統一改成「我」。
+const walker=document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT);
+let node;
+while((node=walker.nextNode())){
+  if(node.nodeValue.includes('你')) node.nodeValue=node.nodeValue.replaceAll('你','我');
+}
